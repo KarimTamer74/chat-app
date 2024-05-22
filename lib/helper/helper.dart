5555ca8bd00dc499dcc58d254bc985df7051c3bc
KarimTamer <kemotamer448@gmail.com>
2024-05-22 23:50:56 +0300

    add bloc package

--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      width: MediaQuery.of(context).size.width/2,
+      width: MediaQuery.of(context).size.width / 2,
       backgroundColor: const Color.fromARGB(96, 158, 158, 158),
       content: Center(child: Text(message)),
     ),
   );
 }
-
- 
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,7 +5,6 @@ import 'package:chatapp/helper/helper.dart';
 import 'package:chatapp/shared_widgets/custom_submit.dart';
 import 'package:chatapp/shared_widgets/custom_title.dart';
 import 'package:chatapp/shared_widgets/custom_text_form_field.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
