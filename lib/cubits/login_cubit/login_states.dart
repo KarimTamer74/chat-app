28350e55729a07f764af61bde59aa069de00e545
KarimTamer <kemotamer448@gmail.com>
2024-05-22 21:15:55 +0300

    create login states

---
+++ lib/cubits/login_cubit/login_states.dart
@@ -0,0 +1,8 @@
+class UserLoginState {}
+class LoginInitialState extends UserLoginState{}
+
+class LoginLoadingState extends UserLoginState{}
+
+class LoginSuccessState extends UserLoginState{}
+
+class LoginErrorState extends UserLoginState{}
