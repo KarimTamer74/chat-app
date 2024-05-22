b2b8bc474f9586db4ef2ea7634c25be6af5d492c
KarimTamer <kemotamer448@gmail.com>
2024-05-23 01:33:21 +0300

    apply bloc consumer for register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,10 +1,13 @@
 import 'package:chatapp/constants/constants.dart';
+import 'package:chatapp/cubits/register_cubit/register_cubit.dart';
+import 'package:chatapp/cubits/register_cubit/register_states.dart';
 import 'package:chatapp/helper/helper.dart';
 import 'package:chatapp/shared_widgets/custom_submit.dart';
 import 'package:chatapp/shared_widgets/custom_title.dart';
 import 'package:chatapp/shared_widgets/custom_text_form_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -22,131 +25,116 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: false,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Create account',
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF2196F3)),
-                  ),
-                ),
-                const Text(
-                  'Please enter your details',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TitleWidget(title: 'Your email'),
-                CustomTextFormField(
-                  obscureText: false,
-                  hint: 'Enter your email',
-                  onChanged: (data) {
-                    email = data;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const TitleWidget(title: 'Password'),
-                CustomTextFormField(
-                  obscureText: true,
-                  hint: 'Enter your password',
-                  onChanged: (data) {
-                    password = data;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const TitleWidget(title: 'Rewrite password'),
-                CustomTextFormField(
-                  obscureText: true,
-                  hint: 'Rewrite password',
-                  onChanged: (data) {
-                    rewritePass = data;
-                  },
-                ),
-                CustomSubmit(
-                    text: '  Register',
-                    onTap: () async {
-                      if (formKey.currentState!.validate()) {
-                        isLoading = true;
-                        setState(() {});
-                        try {
-                          if (rewritePass == password) {
-                            await userRigester();
-                            showSnackBar(context, 'Successfully registered ✔️');
-                            Navigator.pop(context);
-                          } else
-                            showSnackBar(
-                                context, 'Please write the same password.');
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password')
-                            showSnackBar(
-                                context, 'The password provided is too weak.');
-                          else if (e.code == 'email-already-in-use')
-                            showSnackBar(context,
-                                'The account already exists for that email.');
-                        } catch (e) {
-                          showSnackBar(context, 'Error, Please try again');
-                        }
-                        isLoading = false;
-                        setState(() {});
-                      }
-                    }),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return BlocConsumer<UserRegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterLoadingState) {
+          isLoading = true;
+        } else if (state is RegisterSuccessState) {
+          isLoading = false;
+          showSnackBar(context, 'Successfully registered ✔️');
+          Navigator.pop(context);
+        } else if (state is RegisterFailureState) {
+          isLoading = false;
+          showSnackBar(context, state.errorMassage!);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: false,
+          child: Scaffold(
+            backgroundColor: kPrimaryColor,
+            body: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                key: formKey,
+                child: ListView(
                   children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Create account',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF2196F3)),
+                      ),
+                    ),
                     const Text(
-                      'Already have an account?',
+                      'Please enter your details',
                       style: TextStyle(
                           fontSize: 16,
-                          color: Color.fromARGB(255, 238, 241, 243)),
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'LoginPage');
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const TitleWidget(title: 'Your email'),
+                    CustomTextFormField(
+                      obscureText: false,
+                      hint: 'Enter your email',
+                      onChanged: (data) {
+                        email = data;
                       },
-                      child: const Text(
-                        'Login',
-                        style:
-                            TextStyle(color: Color(0xFF2196F3), fontSize: 16),
-                      ),
-                    )
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TitleWidget(title: 'Password'),
+                    CustomTextFormField(
+                      obscureText: true,
+                      hint: 'Enter your password',
+                      onChanged: (data) {
+                        password = data;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TitleWidget(title: 'Rewrite password'),
+                    CustomTextFormField(
+                      obscureText: true,
+                      hint: 'Rewrite password',
+                      onChanged: (data) {
+                        rewritePass = data;
+                      },
+                    ),
+                    CustomSubmit(
+                        text: '  Register',
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {}
+                        }),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Already have an account?',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 238, 241, 243)),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'LoginPage');
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Color(0xFF2196F3), fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
-  }
-
-  Future<void> userRigester() async {
-    final auth = FirebaseAuth.instance;
-
-    await auth.createUserWithEmailAndPassword(
-        email: email!, password: password!);
-    ();
   }
 }
