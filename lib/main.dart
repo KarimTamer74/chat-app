d9738bd492ff6b578eb5ac4ca5d0becdfdb302fb
KarimTamer <kemotamer448@gmail.com>
2024-05-22 23:19:06 +0300

    provide login cubit for all views

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-
+import 'package:chatapp/cubits/login_cubit/login_cubit.dart';
 import 'package:chatapp/views/chat_view.dart';
 import 'package:chatapp/views/home_view.dart';
 import 'package:chatapp/views/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -20,15 +21,23 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        'LoginPage': (context) => LoginPage(),
-        'RegisterPage': (context) => RegisterPage(),
-        'ChatPage': (context) => ChatPage()
-      },
-      initialRoute: 'LoginPage',
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => UserLoginCubit(),
+        ),
+        
+      ],
+      child: MaterialApp(
+        routes: {
+          'LoginPage': (context) => LoginPage(),
+          'RegisterPage': (context) => RegisterPage(),
+          'ChatPage': (context) => ChatPage()
+        },
+        initialRoute: 'LoginPage',
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+      ),
     );
   }
 }
