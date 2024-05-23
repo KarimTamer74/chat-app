d9854893452bfd2e52d7270a532bc80132f288e1
KarimTamer <kemotamer448@gmail.com>
2024-05-23 17:25:02 +0300

    change in chat success state to accept messages list

--- lib/cubits/chat_cubit/chat_state.dart
+++ lib/cubits/chat_cubit/chat_state.dart
@@ -1,8 +1,17 @@
-class ChatState{}
-class ChatInitialState extends ChatState{}
-class ChatLoadingState extends ChatState{}
-class ChatSuccessState extends ChatState{}
-class ChatFailureState extends ChatState{
+import 'package:chatapp/models/message.dart';
+
+class ChatState {}
+
+class ChatInitialState extends ChatState {}
+
+class ChatLoadingState extends ChatState {}
+
+class ChatSuccessState extends ChatState {
+  List<Message> messagesList;
+  ChatSuccessState({required this.messagesList});
+}
+
+class ChatFailureState extends ChatState {
   final String errorMessage;
   ChatFailureState(this.errorMessage);
 }
