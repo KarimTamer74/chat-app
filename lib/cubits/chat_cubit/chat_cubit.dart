2cfa850cfb1cc44f36d952f641f03fd427777603
KarimTamer <kemotamer448@gmail.com>
2024-05-23 16:59:19 +0300

    create send message method to add message to database

--- lib/cubits/chat_cubit/chat_cubit.dart
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -1,6 +1,20 @@
+import 'package:chatapp/constants/constants.dart';
 import 'package:chatapp/cubits/chat_cubit/chat_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitialState());
+  final CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
+  void sendMessage(
+      {required String message,
+      required Object email,
+      required String messageTime}) {
+    messages.add({
+      kMessageTitle: message,
+      kMessageTime: messageTime,
+      kMessageId: email,
+    });
+  }
 }
